6f7036005b1f759d223a45f7e1563695a6e9763e
Gavin D'mello <gavindmello@Gavins-MBP.hsd1.ma.comcast.net>
2022-07-04 19:01:17 -0400

    handled media query handling for generic hover wrapper

--- src/components/generic/useHover.ts
+++ src/components/generic/useHover.ts
@@ -1,12 +1,13 @@
 import  { useState, CSSProperties } from "react";
-import { deflate } from "zlib";
+import { useMediaQuery } from 'react-responsive'
 
 export default function useHover(styleOnHover: CSSProperties, styleOnNotHover: CSSProperties = {})
 {
     const [style, setStyle] = useState(styleOnNotHover);
+    const isTabletOrMobile = useMediaQuery({ query: '(max-width: 1224px)' })
 
-    const onMouseEnter = () => setStyle(styleOnHover)
-    const onMouseLeave = () => setStyle(styleOnNotHover)
+    const onMouseEnter = () => !isTabletOrMobile && setStyle(styleOnHover)
+    const onMouseLeave = () => !isTabletOrMobile && setStyle(styleOnNotHover)
 
     return {style, onMouseEnter, onMouseLeave}
 }
