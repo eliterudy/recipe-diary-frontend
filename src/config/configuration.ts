c8a9177851090b30264453d99915b6c63afe1a58
Gavin D'mello <gavindmello@Gavins-MBP.hsd1.ma.comcast.net>
2022-07-15 16:46:26 -0400

    removed deleted icons

--- src/config/configuration.ts
+++ src/config/configuration.ts
@@ -4,10 +4,7 @@ const colorCodes: Object = {
 
 
 const icons: any = {
-    heart_filled: require("../assets/icons/heart_filled.png"),
-    heart_unfilled: require("../assets/icons/heart_unfilled.png"),
-    star_filled: require("../assets/icons/star_filled.png"),
-    star_unfilled: require("../assets/icons/star_unfilled.png"),
+   
     bookmark_selected: require("../assets/icons/bookmark_selected.png"),
     bookmark_hover: require("../assets/icons/bookmark_hover.png"),
     bookmark_unselected: require("../assets/icons/bookmark_unselected.png"),
