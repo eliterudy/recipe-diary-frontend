c1eaf5a40cbac14353289221ec2a5df5dd22fe89
Gavin D'mello <gavindmello@Gavins-MBP.hsd1.ma.comcast.net>
2022-08-04 21:49:37 -0400

    minor fix

--- src/App.tsx
+++ src/App.tsx
@@ -1,18 +1,18 @@
 import React from 'react';
 import './App.css';
 import Routes from './components/routes';
-import {BrowserRouter} from 'react-router-dom';
+import {HashRouter} from 'react-router-dom';
 import {Provider} from 'react-redux';
 import {store} from './redux/config/configureStore';
 
 function App() {
   return (
     <Provider store={store}>
-      <BrowserRouter>
+      <HashRouter basename="/recipe-diary-frontend">
         <div>
           <Routes />
         </div>
-      </BrowserRouter>
+      </HashRouter>
     </Provider>
   );
 }
