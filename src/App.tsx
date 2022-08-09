7fe07bbcab9560cd4d1002f88300c4cb850249c9
Gavin D'mello <gavindmello@Gavins-MBP.hsd1.ma.comcast.net>
2022-08-09 19:13:58 -0400

    deployment tweaks

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,6 @@
 import React from 'react';
 import './App.css';
 import Routes from './components/routes';
-import {HashRouter} from 'react-router-dom';
 import {Provider} from 'react-redux';
 import {store} from './redux/config/configureStore';
 
