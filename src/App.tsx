1bf27ed8f0f5bd2b3c8e26bb6f5e3f9b260d5740
Gavin D'mello <gavindmello@Gavins-MBP.hsd1.ma.comcast.net>
2022-08-09 18:58:09 -0400

    deployment tweaks

--- src/App.tsx
+++ src/App.tsx
@@ -1,18 +1,18 @@
 import React from 'react';
 import './App.css';
 import Routes from './components/routes';
-import {BrowserRouter} from 'react-router-dom';
+import {HashRouter} from 'react-router-dom';
 import {Provider} from 'react-redux';
 import {store} from './redux/config/configureStore';
 
 function App() {
   return (
     <Provider store={store}>
-      <BrowserRouter basename={process.env.PUBLIC_URL}>
+      <HashRouter basename={process.env.PUBLIC_URL}>
         <div>
           <Routes />
         </div>
-      </BrowserRouter>
+      </HashRouter>
     </Provider>
   );
 }
